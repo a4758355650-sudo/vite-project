477874bedd9d68b88c77ebe96d0efef0ba3dce02
bobo <a4758355650@gmail.com>
2025-11-27 15:58:09 +0800

    css

--- src/App.tsx
+++ src/App.tsx
@@ -4,50 +4,32 @@ import './App.css'
 import Layout from './components/Layout'
 
 function App() {
-  const [] = useState(0)
-
+  
+  const signlist = [
+    '%',"CE","C","⌫",
+    '1/x',"x²","√x"," ÷",
+    '7',"8","9","×",
+    '4',"5","6","-",
+    '1',"2","3","+",
+    '±',"0",".","=",
+  ];
+ 
+ 
+  const [count,setCount] = useState(0)
   return (
     <Layout>
-      
+      <div className="grid grid-cols-4 gap-6 bg-gray-100 p-4 rounded-x1 shadow-2x1 max-w-sm ma-auto">
+        {signList.map((sing, index) => (
+          
+         return( 
+          <div key={index} className={baseClasses}>
+            {sing}
+            </div>       
+         )
+      )
+        )
+        }
 
-
-      <div className="grid grid-cols-4 gap-6">
-        {/* 這裡面的子元素會自動變成 3 欄排列 */}
-        
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
       </div>
     </Layout>
   )
--- src/MyView.tsx
+++ src/MyView.tsx
@@ -1,13 +1,31 @@
+
 import React from 'react';
 
-
+interface Pet {
+  name: string;
+  id: number;
+  title:string;
+}
+const pets:Pet[] = [
+    {id:101 , title:"dog", name:"lucky"},
+    {id:102 , title:"cat", name:"kitty"},
+];
 
 export const MyView: React.FC = () => {
 
 
-  return (
-    <div className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50 transition-opacity duration-300">
-
+ return (
+    <div className="p-4 border rounded">
+      <h3>Fruit List</h3>
+      <ul>
+        {/* map 的用法不變，但現在 TS 知道 'fruit' 是一個 string */}
+        {pets.map((pet, index) => (
+          <div key={pet.id}>
+            <span>{pet.title}</span>
+            <input type="text" value={pet.name} />
+          </div>
+        ))}
+      </ul>
     </div>
   );
 };
